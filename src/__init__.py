51937553ce6be586574788f8df4cfdc95f94e68c
vasilisvyth <72560545+vasilisvyth@users.noreply.github.com>
2023-08-25 20:04:02 +0300

    Update src/__init__.py
    Co-authored-by: Gergely Papp <gergopool@users.noreply.github.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from .static import Logger, GlobalState
 
-TRAINING_SAMPLES = 1.4 * 10**6
-VALIDATION_SAMPLES = 1 * 10**5
-TEST_SAMPLES = 1 * 10**5
+WIKI_TRAINING_SAMPLES = 1e5
+WIKI_VALIDATION_SAMPLES = 1e4
+WIKI_TEST_SAMPLES = 1e4
 
 LANGUAGES = ['es']#['en', 'de', 'fr', 'es', 'zh']
